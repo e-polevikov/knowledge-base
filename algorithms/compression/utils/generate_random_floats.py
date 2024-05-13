cba0f0e52c249491eb18037adff56b831963eff9
Eugene Polevikov <eugene.polevikov@yandex.ru>
2024-05-13 07:43:59 +0300

    More updates

--- algorithms/compression/utils/generate_random_floats.py
+++ algorithms/compression/utils/generate_random_floats.py
@@ -3,7 +3,11 @@ import numpy as np
 
 if __name__ == "__main__":
     mu, sigma = 3, 4.0 # mean and standard deviation
-    s = np.float32(np.random.normal(mu, sigma, 2 * 10**6))
+
+    num_floats = 2**24
+    print(num_floats)
+
+    s = np.float32(np.random.normal(mu, sigma, num_floats))
     s = np.sort(s)
 
     with open("floats.bin", "wb") as f:
--- algorithms/compression/utils/generate_random_permutation.py
+++ algorithms/compression/utils/generate_random_permutation.py
@@ -2,11 +2,10 @@ import random
 
 
 if __name__ == "__main__":
-    permutation = list(range(2**10))
+    permutation = list(range(2**24))
 
     with open("permutation.bin", "wb") as f:
-        for _ in range(1000):
-            random.shuffle(permutation)
-            
-            for value in permutation:
-                f.write(value.to_bytes(length=2, byteorder='little'))
+        random.shuffle(permutation)
+        
+        for value in permutation:
+            f.write(value.to_bytes(length=3, byteorder='little'))
